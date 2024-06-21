9924ccdffe30043fc03583c5a972b854ea247814
Caner Mollaoğlu <canermollaoglu@hotmail.com>
2024-06-21 03:15:13 +0300

    yeni mesaj commit

--- WinFormsApp4/Form1.cs
+++ WinFormsApp4/Form1.cs
@@ -14,5 +14,10 @@ namespace WinFormsApp4
             string test = "deneme";
             int a = 6;
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string mesaj = $"Yeni bir mesaj yazýyoruz.";
+        }
     }
 }
